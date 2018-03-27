c55150fb6947a496b8b25f1d610700f0cf0a0e2c
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-27 10:31:38 -0700

    Added Sample from Xamarin Blog

--- XamarinBlobStorageApp/App.cs
+++ XamarinBlobStorageApp/App.cs
@@ -1,9 +1,59 @@
 ﻿using Xamarin.Forms;
+using System.Linq;
 
 namespace XamarinBlobStorageApp
 {
     public class App : Application
     {
-        public App() => MainPage = new BaseNavigationPage(new PhotoListPage());
+        //public App() => MainPage = new BaseNavigationPage(new PhotoListPage());
+        public App()
+        {
+            MainPage = new NavigationPage(new ImagePage());
+        }
+    }
+
+    public class ImagePage : ContentPage
+    {
+        readonly Label _title = new Label
+        {
+            HorizontalTextAlignment = TextAlignment.Center,
+            TextColor = ColorConstants.DetailColor
+        };
+        readonly Image _image = new Image();
+        readonly ActivityIndicator _activityIndicator = new ActivityIndicator();
+
+        public ImagePage()
+        {
+            Content = new StackLayout
+            {
+                Spacing = 15,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    _title,
+                    _image,
+                    _activityIndicator
+                }
+            };
+
+            Title = "Image Page";
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            _activityIndicator.IsRunning = true;
+
+            var photoList = await PhotosBlobStorageService.GetPhotos();
+
+            var firstPhoto = photoList?.FirstOrDefault();
+
+            _title.Text = firstPhoto?.Title;
+            _image.Source = ImageSource.FromUri(firstPhoto?.Uri);
+
+            _activityIndicator.IsRunning = false;
+            _activityIndicator.IsVisible = false;
+        }
     }
 }
