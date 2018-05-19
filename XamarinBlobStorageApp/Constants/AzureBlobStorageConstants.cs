07a01510203c66d86e8b9879714dbfc8cedcd71c
Brandon Minnick <brminnick@users.noreply.github.com>
2018-05-19 10:03:44 -0700

    Create README.md

---
+++ XamarinBlobStorageApp/Constants/AzureBlobStorageConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace XamarinBlobStorageApp
+{
+    public static class AzureBlobStorageConstants
+    {
+#error Missing Blob Storage Connection String
+		//Create an Azure Blob Storage instance here: https://aka.ms/AzureBlobStorage
+		public const string ConnectionString = "Enter Connection String Here";
+        public const string ContainerName = "photos";
+    }
+}
